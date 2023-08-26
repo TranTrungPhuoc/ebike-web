c99d9de33dd3af4e86cb6bfb75f81f971dcc62d7
Trần Trung Phước <trantrungphuoc@MacBook-Air-cua-Tran-2.local>
2023-08-26 16:17:19 +0700

    add tailwind vs flowbite vs sass

--- app/(layout)/home/header.tsx
+++ app/(layout)/home/header.tsx
@@ -1,7 +1,33 @@
+import { SvgPhone } from "../component/svg/svgPhone";
 export function Header() {
   return (
     <header>
-      hello
+      <div className="container mx-auto">
+        <div className="flex flex-row">
+          <div className="basis-1/5 flex">
+            <div className="svg-phone flex-none">
+              <SvgPhone />
+            </div>
+            <div className="buy-phone grow ml-2 uppercase">
+              Mua hàng: 028 6253 9118
+            </div>
+          </div>
+          <div className="basis-2/5 flex">
+            <div className="svg-phone flex-none">
+              <SvgPhone />
+            </div>
+            <div className="guarante-phone grow ml-2 uppercase">
+              Bảo hành: 028 6253 9118
+            </div>
+          </div>
+          <div className="basis-1/5">
+            <div className="uppercase">The work Ebike.com</div>
+          </div>
+          <div className="basis-1/5 uppercase">
+            <div className="uppercase">Bảo hành 2 năm</div>
+          </div>
+        </div>
+      </div>
     </header>
   );
 }
--- app/(layout)/layout.tsx
+++ app/(layout)/layout.tsx
@@ -1,3 +1,4 @@
+"use client"
 import { Header } from "./home/header";
 export default function RootLayout({
     children,
